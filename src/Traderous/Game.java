441efccd7658fbedaf4abee6cf3819d1d60341c3
dayliss <henrysmith.eshs@gmail.com>
2014-12-08 18:25:00 -0800

    Created Game class with main method

---
+++ src/Traderous/Game.java
@@ -0,0 +1,14 @@
+package Traderous;
+
+/**
+ * Created by Henry Smith on 12/8/2014.
+ */
+public class Game {
+    /**
+     * Main method and initializer for the whole program
+     */
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+
+}
